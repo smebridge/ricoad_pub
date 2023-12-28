c7bcf9e6e66b8a406498b2a0a77f3fed3c95d75c
smebridge <155061757+smebridge@users.noreply.github.com>
2023-12-28 13:48:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ centerx=(WIDTH//2)
 centery=(HEIGHT//2)
 
 def checkVER():
-    firmware_url = "https://github.com/smebridge/ricoad_pub"
+    firmware_url = "https://github.com/smebridge/ricoad_pub/main/"
 
     ota_updater = OTAUpdater(SSID, PSK, firmware_url, "main.py")
 
